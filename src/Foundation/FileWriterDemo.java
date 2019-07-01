bb8a96b0d1c10e2c88377ade973acc028ea0e81f
Regcookies <reggiewangyj1@gmail.com>
2019-07-01 13:52:21 +0800

    add some shit

--- src/Foundation/FileWriterDemo.java
+++ src/Foundation/FileWriterDemo.java
@@ -3,6 +3,14 @@ package Foundation;
 import java.io.FileWriter;
 import java.io.IOException;
 
+
+
+/*
+   java 中的命名规范:
+   1: 类, 接口：大驼峰
+   2: 变量, 方法： 小驼峰
+   3: 常量名: 所有字符都大写，用下划线想链接
+ */
 public class FileWriterDemo {
     public static void main(String[] args) throws IOException {
         FileWriter f = new FileWriter("a.txt", true);

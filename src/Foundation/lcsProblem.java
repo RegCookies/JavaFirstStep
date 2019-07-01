e00772bce5fd7edb508b056405decdd08596894c
Regcookies <reggiewangyj1@gmail.com>
2019-07-01 11:34:15 +0800

    i do not know wwhat happened

---
+++ src/Foundation/lcsProblem.java
@@ -0,0 +1,12 @@
+package Foundation;
+
+public class lcsProblem {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 8, 5, 7, 9};
+        findLongestSub(arr);
+
+    }
+    public static void findLongestSub(int[] array){
+        return;
+    }
+}
